e89498a02fceba65670c7466cb08b6971088a22b
Sukhmeet Singh <sing1539@algonquinlive.com>
2022-02-08 09:49:28 -0500

    added my grrid layout, removed...

--- app/src/main/java/com/cst2335/sing1539/MainActivity.java
+++ app/src/main/java/com/cst2335/sing1539/MainActivity.java
@@ -1,14 +1,45 @@
 package com.cst2335.sing1539;
 
+import static com.cst2335.sing1539.R.id.togglebutton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    //LinearLayout mainland;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+        setContentView(R.layout.activity_main_grid);
+
+    public void display_snack (View v) {
+
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch toggle = (Switch) findViewById(togglebutton);
+
+        toggle.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+            Snackbar snack;
+            if (isChecked) {
+                snack = Snackbar.make(toggle, "The switch is now followed by On", Snackbar.LENGTH_LONG);
+
+            }
+            else {
+                snack = Snackbar.make(toggle, "The switch is now followed by off", Snackbar.LENGTH_LONG);
+
+            }
+            snack.setAction("Undo", click -> compoundButton.setChecked(false));
+            snack.show();
+
+        })
+        ;}}
